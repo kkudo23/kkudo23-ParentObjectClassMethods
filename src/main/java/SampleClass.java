6c1f6526c16893e1c4773e59382ab75f8bcc1ebc
Ken Kudo <kkudo224@gmail.com>
2023-03-25 09:17:18 +0000

    Done

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -56,9 +56,13 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
-
+    public boolean equals(SampleClass other) {
+        return this.a == other.a && this.b == other.b;
+    }
 
     //    implement a custom .toString(){} method here.
-
+    public String toString() {
+        return "SampleClass{a="+ this.a + ", b=" + this.b + "}";
+    }
     
 }
